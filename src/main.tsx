470ab6a8ca12de52eb415f3c0d4025d6ffbc2993
openHacking <openHacking@126.com>
2024-09-15 23:42:06 +0800

    ads

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,11 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <footer style={{ textAlign: 'center', marginTop: '20px' }}>
+      关注微信公众号：
+      <a href="https://mp.weixin.qq.com/s/G7wu6WfpdqpcZz3OnHHR6w" target="_blank" rel="noopener noreferrer">
+        技术分社
+      </a>
+    </footer>
   </StrictMode>,
 )
